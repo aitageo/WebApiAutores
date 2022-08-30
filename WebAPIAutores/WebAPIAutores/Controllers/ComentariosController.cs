3689edf805975581ba6a2c701c70bd9aabe18f80
aitageo <aitageo1980@hotmail.com>
2022-08-29 22:47:57 -0500

    Adicionando Comentarios

---
+++ WebAPIAutores/WebAPIAutores/Controllers/ComentariosController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.DTOs;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros/{libroId:int}/comentarios")]
+    public class ComentariosController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ComentariosController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(int libroId, ComentarioCreacionDTO comentarioCreacionDTO )
+        {
+            var existeLibro = await context.Libros.AnyAsync(libroDb => libroDb.Id == libroId);
+            if (!existeLibro)
+            {
+                return NotFound();
+            }
+            var comentario = mapper.Map<Comentario>(comentarioCreacionDTO);
+            comentario.LibroId = libroId;
+            context.Add(comentario);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
